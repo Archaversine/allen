f7aa7e9f469b543d0bf6dd7261b1a9ee39eb6c8e
Archaversine <adam.brohl.w@gmail.com>
2023-07-21 10:26:55 -0500

    Refactored hasConstraint function

--- src/Data/Allen/Relation.hs
+++ src/Data/Allen/Relation.hs
@@ -16,10 +16,8 @@ inverse :: RelationBits -> RelationBits
 inverse r = snd $ head $ filter ((== r) . fst) inverseLookup
 
 hasConstraint :: Relation -> IntervalID -> IntervalID -> Allen Bool
-hasConstraint (toBits -> r) a b = do 
-    i1 <- fromID a
-
+hasConstraint (toBits -> r) (fromID -> a) b = do 
     let match (bits, i) | i == b = (r .&. bits) /= 0 
                         | otherwise = False
 
-    return $ any match $ intervalRelations i1 
+    any match . intervalRelations <$> a
