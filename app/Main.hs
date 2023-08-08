709bc9ae34a55e244f39bd5fef508571e6071450
Archaversine <adam.brohl.w@gmail.com>
2023-08-08 13:30:36 -0400

    Added `Command`s

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,36 @@ module Main (main) where
 
 import Data.Allen
 
+data Command = CreateInterval String 
+             | AssumeRelation String String RelationBits
+             | GetConstraints String String
+             | ShowGraph
+             | ResetGraph
+             | Quit
+             | InvalidCommand
+
+parseCommand :: String -> Command 
+parseCommand str = case words str of 
+    [ "create", name      ] -> CreateInterval name 
+    [ "assume", a, rel, b ] -> AssumeRelation a b (bitsFromString rel)
+    [ "constraints", a, b ] -> GetConstraints a b 
+    [ "graph"             ] -> ShowGraph 
+    [ "clear"             ] -> ResetGraph 
+    [ "quit"              ] -> Quit 
+    [ "exit"              ] -> Quit
+    _                       -> InvalidCommand
+
+readCommand :: IO Command 
+readCommand = do 
+    putStr "Allen > "
+    command <- parseCommand <$> getLine
+
+    case command of 
+        InvalidCommand -> do 
+            putStrLn "Invalid command"
+            readCommand
+        _ -> return command
+
 calc :: Allen ([Relation], [Relation]) 
 calc = do 
     a <- interval 
